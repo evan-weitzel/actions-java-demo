1bb2ccb4d4763e58099956a2a8cc72429b8c372a
evan-weitzel <evandane2005@gmail.com>
2026-02-18 19:20:44 -0500

    Add string length to Hello

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
 	public static void main(String[] args) {
+		String text = null;
 		System.out.println("Hello from GitHub Actions");
+		System.out.printf("String length: %d\n", text.length());
 	}
 }
